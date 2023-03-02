79b174e48a36975fd25152acee389ae32fb79466
steaby <126570185+steaby@users.noreply.github.com>
2023-03-02 07:22:57 -0800

    Add files via upload

--- Practical/Animal.java
+++ Practical/Animal.java
@@ -1,6 +1,6 @@
-package multilevelinheritance;
+package package1;
 //creating a parent class
-class Animal
+ class Animal
 {
 	// creating a method 
    public void display()
@@ -8,22 +8,13 @@ class Animal
 	System.out.println("Inside parent class");  
    }
 }
-// creating a child class for the above parent class
-class Cat extends Animal
+//creating a child class for the above parent class
+class Dog extends Animal
 {
 	// creating a method
 	public void sound()
 	{
-		System.out.println("Meow...");
-	}
-}
-//creating a child class for the cat class which is a parent class 
-class Kitten extends Cat
-{
-	//creating a method
-	public void body()
-	{
-		System.out.println("kittens have belly buttons");
+		System.out.println("Barks...");
 	}
 }
 class inheritancetester
@@ -31,10 +22,8 @@ class inheritancetester
 	public static void main (String[] args)
 	{
 		//creating a object
-		Kitten c = new Kitten();
-		c.display();
-		c.sound();
-		c.body();
-		
+		Dog d = new Dog();
+		d.display();
+		d.sound();
 	}
 }
