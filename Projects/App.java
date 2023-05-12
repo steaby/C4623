08f7b3888140f49a02106e88e4a1d03d9cbd2acb
steaby <126570185+steaby@users.noreply.github.com>
2023-05-12 05:29:24 -0700

    Add files via upload

--- Projects/App.java
+++ Projects/App.java
@@ -1,36 +1,13 @@
-package com.bloodbank.main;
+package com.maven;
 
-import com.bloodbank.Dao.BloodDonorDao;
-import com.bloodbank.model.BloodDonor;
-
-
+/**
+ * Hello world!
+ *
+ */
 public class App 
 {
-
-	public static void main(String[] args)
-	{
-		BloodDonorDao dao = new BloodDonorDao();
-		BloodDonor donor =new BloodDonor();
-		BloodDonor donor1 = new BloodDonor();
-		BloodDonor donor2 = new BloodDonor();
-		donor.setB_ID(12);
-		donor.setB_Grp("A+");
-		donor1.setB_ID(14);
-		donor1.setB_Grp("O+");
-		donor2.setB_ID(41);
-		donor2.setB_Grp("AB-");
-		
-		BloodDonorDao.saveDonor(donor);
-		BloodDonorDao.saveDonor(donor1);
-		BloodDonorDao.saveDonor(donor2);
-		
-		donor1.setB_ID(65);
-		BloodDonorDao.saveOrUpdateDonor(donor1);
-		
-		BloodDonorDao.deletedonor(donor2);
-		
-
-
-	}
-
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
 }
